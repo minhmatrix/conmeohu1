25fc8cd11818e93b04b9497eca2a4bab23d0335f
megait004 <giapfc123@gmail.com>
2025-09-08 12:19:01 +0700

    ...

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '7696170315:AAHzY3ANCN23bED-vqRYC_3-49Ura_YOycA',
-    chat_id: '7211586401',
+    token: '',
+    chat_id: '',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
